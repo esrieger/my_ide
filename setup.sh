b86f29837425124c7a2c2b4ee4c4b8445e2ed2d3
Erik Rieger <riegererik0@gmail.com>
2021-06-01 09:17:59 -0400

    Install cmake clangd and latest gcc

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt update
+
 cp .vimrc ~/
 cp .bashrc ~/
 cp .tmux.conf ~/
@@ -30,13 +32,20 @@ git pull
 wget -O ~/.vim/cscope/cscope_maps.vim "cscope.sourceforge.net/cscope_maps.vim"
 
 sudo apt install libncurses5-dev libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python2-dev python3-dev ruby-dev lua5.2 liblua5.2-dev libperl-dev git
-sudo apt install build-essential cmake tmux
+sudo apt install build-essential tmux libssl-dev clang
 sudo apt remove vim vim-runtime gvim
-git clone https://github.com/vim/vim.git ~/projects
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt install gcc-7 g++-7 gcc-8 g++-8 gcc-9 g++-9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90 --slave /usr/bin/g++ g++ /usr/bin/g++-9 --slave /usr/bin/gcov gcov /usr/bin/gcov-9
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80 --slave /usr/bin/g++ g++ /usr/bin/g++-8 --slave /usr/bin/gcov gcov /usr/bin/gcov-8
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70 --slave /usr/bin/g++ g++ /usr/bin/g++-7 --slave /usr/bin/gcov gcov /usr/bin/gcov-7
+git clone https://github.com/vim/vim.git ~/projects/vim
 cd ~/projects/vim
 ./configure --with-features=huge --enable-multibyte --enable-rubyinterp=yes --enable-python3interp=yes --with-python3-config-dir=$(python3-config --configdir) --enable-perlinterp=yes --enable-luainterp=yes --enable-gui=gtk2 --enable-cscope --prefix=/usr/local
 make VIMRUNTIMEDIR=/usr/local/share/vim/vim82
-sudo make install
+git clone https://github.com/Kitware/CMake.git ~/projects/CMake
+cd ~/projects/CMake
+./bootstrap && make && sudo make install
 git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
 cd ~/.tmux/plugins/tpm
 git pull
@@ -44,6 +53,6 @@ git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/YouComplet
 cd ~/.vim/bundle/YouCompleteMe
 git pull
 git submodule update --init --recursive
-python3 install.py --clangd-completer
+sudo python3 install.py --clangd-completer
 
 echo "**** RUN Prefix + I to install tmux plugins !!! ***"
